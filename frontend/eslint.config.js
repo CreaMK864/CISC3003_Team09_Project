0abbc2279a5d5dbf33a557fca3b2cbf5fb9b365f
Glinte <96855131+Glinte@users.noreply.github.com>
2025-04-20 17:06:35 +0800

    Add more frontend linting / formatting tools

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import globals from "globals";
 import js from "@eslint/js";
+import html from "@html-eslint/eslint-plugin";
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    rules: {
+      ...html.configs["flat/recommended"].rules, // Must be defined. If not, all recommended rules will be lost
+      "@html-eslint/indent": ["error", 2],
+      "@html-eslint/require-closing-tags": ["error", { selfClosing: "always" }],
+      "@html-eslint/no-extra-spacing-attrs": [
+        "error",
+        { enforceBeforeSelfClose: true },
+      ],
+    },
+  },
+  eslintConfigPrettier,
 ]);
