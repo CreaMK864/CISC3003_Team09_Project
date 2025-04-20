b3933bc184879f6bba7dd4f0cdff3cc50c0a556c
Glinte <96855131+Glinte@users.noreply.github.com>
2025-04-20 22:00:29 +0800

    Change API_BASE_URL to https://api.saviomak.com

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -4,4 +4,4 @@ export const SUPABASE_URL = "https://rktloofzzbabyeugqots.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrdGxvb2Z6emJhYnlldWdxb3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3OTgyNjMsImV4cCI6MjA2MDM3NDI2M30.f992jGcCkSeX7qKwCQ7viJ-3i7IQXdakx2YD4rJ9UGc";
 /** @type {string} */
-export const API_BASE_URL = "http://localhost:8000"; // TODO: Change to "https://api.saviomak.com" in production
+export const API_BASE_URL = "https://api.saviomak.com"; // Change to http://localhost:8000 for local development
