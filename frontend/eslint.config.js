c1c382168944ad785650b3810f55353fa2e2d84e
Glinte <96855131+Glinte@users.noreply.github.com>
2025-05-05 09:56:18 +0800

    Ignore attrs-newline error

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -25,6 +25,7 @@ export default defineConfig([
         "error",
         { enforceBeforeSelfClose: true },
       ],
+      "@html-eslint/attrs-newline": "off",
     },
   },
   eslintConfigPrettier,
