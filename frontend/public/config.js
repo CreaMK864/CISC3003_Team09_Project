4a113bde2b68f81d61243a23d8681256e21be6bc
Glinte <96855131+Glinte@users.noreply.github.com>
2025-05-06 17:32:02 +0800

    Update supabase url and anon key

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,7 +1,7 @@
 /** @type {string} */
-export const SUPABASE_URL = "https://rktloofzzbabyeugqots.supabase.co";
+export const SUPABASE_URL = "https://edlrvbzhxvyvukjmvkof.supabase.co";
 /** @type {string} */
 export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrdGxvb2Z6emJhYnlldWdxb3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3OTgyNjMsImV4cCI6MjA2MDM3NDI2M30.f992jGcCkSeX7qKwCQ7viJ-3i7IQXdakx2YD4rJ9UGc";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbHJ2YnpoeHZ5dnVram12a29mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MjIxMDQsImV4cCI6MjA2MjA5ODEwNH0.PlxiJPCsvYbGqA2h_jiFDPu8_AvdPQ_C01Qw2AZ9DDo";
 /** @type {string} */
 export const API_BASE_URL = "https://api.saviomak.com"; // Change to http://localhost:8000 for local development
