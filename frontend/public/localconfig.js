fac22280562ea0f4af1a2a8cbfc0d085bba045c9
Glinte <96855131+Glinte@users.noreply.github.com>
2025-05-11 13:19:59 +0800

    Remove unused JS files

--- frontend/public/localconfig.js
+++
@@ -1,7 +0,0 @@
-/** @type {string} */
-export const SUPABASE_URL = "http://localhost:8000";
-/** @type {string} */
-export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzQ1MDc4NDAwLCJleHAiOjE5MDI4NDQ4MDB9.CqYBh5GwRneprfs6NnffSWpw-mS2ToHHHPYsFpAoBvw";
-/** @type {string} */
-export const API_BASE_URL = "https://api.saviomak.com"; // Change to http://localhost:8000 for local development
