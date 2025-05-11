5fccdb1c28bd0cfb81c9db70ffccb723053388bd
dc20758 <dc20758@umac.mo>
2025-05-11 15:58:47 +0800

    1

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -133,7 +133,22 @@ export async function get_username(uid) {
     console.error("There was a problem with the fetch operation:", error);
   }
 }
+/**
+ * Get username from user ID
 
+ 
+async function get_title(cid) {
+    try {
+    const response = await fetch(`${API_BASE_URL}/conversations`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data[cid].title;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
 /**
  * Update authentication UI based on user state
  * @param {any} user
@@ -255,14 +270,16 @@ async function getOrCreateConversation() {
     }
 
     const conversations = await response.json();
+    //  const url = new URL(window.location.href);
+    //   const conversationId = url.searchParams.get('conversationId');
 
     if (conversations.length > 0) {
-      currentConversationId = conversations[0].id;
       const chatHeader = /** @type {HTMLElement} */ (
         document.querySelector(".chat-header h1")
       );
       if (chatHeader) {
         chatHeader.textContent = conversations[0].title;
+        // chatHeader.textContent = await get_title(conversationId);
       }
     } else {
       await createNewConversation();
@@ -281,7 +298,7 @@ async function getOrCreateConversation() {
     );
     currentConversationId = 1;
     const chatHeader = /** @type {HTMLElement} */ (
-      document.querySelector(".chat-header h2")
+      document.querySelector(".chat-header h1")
     );
     if (chatHeader) {
       chatHeader.textContent = "Default Conversation";
@@ -320,7 +337,7 @@ async function createNewConversation() {
     const conversation = await response.json();
     currentConversationId = conversation.id;
     const chatHeader = /** @type {HTMLElement} */ (
-      document.querySelector(".chat-header h2")
+      document.querySelector(".chat-header h1")
     );
     if (chatHeader) {
       chatHeader.textContent = conversation.title;
